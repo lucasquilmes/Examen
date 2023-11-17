abac8d1e7d8d7dd7b3f9ab6ef90dfc3626ea274d
lucas dening <you@example.com>
2023-11-17 09:20:06 +0100

    d

--- code.js
+++ code.js
@@ -1,19 +1,25 @@
-const SWAPI_BASE_URL = 'https://wizard-world-api.herokuapp.com';
+fetch(`http://hp-api.herokuapp.com/api/characters/house/Gryffindor`).then((data)=>{
+    let info = data.json();
+    return info;
+}).then((data)=>{
+    displaydata(data);
+})
 
-window.onload = async () => {
 
-    const films = await getAllStarWarsFilms();
-  
-    const spinnerHtmlElement = document.getElementById('spinner');
-    spinnerHtmlElement.remove();
-  
-    for (const wizard of wizards) {
-      const mainHtmlElement = document.getElementById('main');
-      const newElement = document.createElement('div');
-      newElement.innerHTML = `
-        <h2>${wizard.FirstName}</h2>
-        <p>${{wizard.LastName}</p>
-      `;
-      mainHtmlElement.appendChild(newElement);
-    }
-  };
+function displaydata(data){
+    console.log(data);
+    var main = document.querySelector(".main");
+    for(var i = 0;i<data.length;i++){
+        console.log(data[i]);
+        let box = document.createElement("div");
+        box.setAttribute("class","box");
+        let content =document.getElementById("content");
+
+        main.appendChild(box);
+        box.innerHTML = `<h2>${data[i].name}</h2>
+                        <img src="${data[i].image}"></img>
+                        <div class="info">
+                            <p>Gender:- ${data[i].gender}</p>
+                            <p>House:- ${data[i].house}</p>
+                            <p>DateOfBirth:- ${data[i].dateOfBirth}</p>
+                        </div>`
