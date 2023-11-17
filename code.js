593d41f6c53ab94825f14ed3c4505b6f81ba22c7
lucas dening <you@example.com>
2023-11-17 08:48:13 +0100

    12

--- code.js
+++ code.js
@@ -1,1 +1,19 @@
 const SWAPI_BASE_URL = 'https://wizard-world-api.herokuapp.com';
+
+window.onload = async () => {
+
+    const films = await getAllStarWarsFilms();
+  
+    const spinnerHtmlElement = document.getElementById('spinner');
+    spinnerHtmlElement.remove();
+  
+    for (const film of films) {
+      const mainHtmlElement = document.getElementById('main');
+      const newElement = document.createElement('div');
+      newElement.innerHTML = `
+        <h2>${film.title}</h2>
+        <p>${film.opening_crawl}</p>
+      `;
+      mainHtmlElement.appendChild(newElement);
+    }
+  };
